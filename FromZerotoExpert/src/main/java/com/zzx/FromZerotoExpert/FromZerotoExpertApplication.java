5f6fb958d3421bf19f738a4b9c91e2ff8676b4bb
zzxjava <h1208532151@126.com>
2022-09-14 12:49:20 +0800

    cookie

---
+++ FromZerotoExpert/src/main/java/com/zzx/FromZerotoExpert/FromZerotoExpertApplication.java
@@ -0,0 +1,13 @@
+package com.zzx.FromZerotoExpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromZerotoExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromZerotoExpertApplication.class, args);
+	}
+
+}
---
+++ FromZerotoExpert/src/test/java/com/zzx/FromZerotoExpert/FromZerotoExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzx.FromZerotoExpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromZerotoExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
